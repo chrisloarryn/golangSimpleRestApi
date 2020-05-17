7d573271fd9461c0c6b2aa61d6964afbd5839da4
chrisloarryn <chrisloarryn@gmail.com>
2020-05-17 00:29:56 -0400

    @GetAllTasks and @CreateOneTask

---
+++ Readme.md
@@ -0,0 +1,10 @@
+gorilla/mux
+- ``github.com/gorilla/mux``
+// for routes
+
+Compile and exec 
+- ``CompileDaemon -command="./goSimpleRestApi"
+``
+
+``github.com/githubnemo/CompileDaemon``
+<p> for compile/ like nodemon </p>
--- main.go
+++ main.go
@@ -1,29 +1,60 @@
 package main
 
-import ("fmt"
-		"github.com/gorilla/mux"
-		"net/http"
-		"log"	
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"io/ioutil"
+	"log"
 )
 
 type task struct {
-	ID int `json:ID`
-	Name string `json:Name`
+	ID      int    `json:ID`
+	Name    string `json:Name`
 	Content string `json:Content`
 }
 
 type allTasks []task
 
-var tasks = allTasks {
+var tasks = allTasks{
 	{
-		ID: 1,
-		Name: "Task One",
+		ID:      1,
+		Name:    "Task One",
+		Content: "Some content",
+	},
+	{
+		ID:      2,
+		Name:    "Task Two",
 		Content: "Some content",
 	},
 }
 
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Please insert a valid task!")
+	}
+	json.Unmarshal(reqBody, &newTask)
+
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	// data type / StatusCreated to give feedback / create task
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
 func indexRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my API")
+	fmt.Fprintf(w, "Welcome to my REST API jj")
 }
 
 func main() {
@@ -31,6 +62,8 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks", createTask).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
