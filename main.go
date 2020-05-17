0dbd06e6cd68c8871c66096f292263387bc60706
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2020-05-16 21:59:47 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import ("fmt")
+import ("fmt"
+		"github.com/gorilla/mux"
+		"net/http"
+		"log"	
+)
+
+type task struct {
+	ID int `json:ID`
+	Name string `json:Name`
+	Content string `json:Content`
+}
+
+type allTasks []task
+
+var tasks = allTasks {
+	{
+		ID: 1,
+		Name: "Task One",
+		Content: "Some content",
+	},
+}
+
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my API")
+}
 
 func main() {
-	fmt.Println("Hello from GoLang")
+	// fmt.Println("Hello from GoLang")
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", indexRoute)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
